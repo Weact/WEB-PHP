dbe16a40cabcca11cf99711a9a9bcaf9399618a0
Weaact <lucas.druckes@numericable.fr>
2020-03-08 15:48:23 +0100

    yapadkass comments

--- VirtualHost_Yapadkass/index.php
+++ VirtualHost_Yapadkass/index.php
@@ -6,13 +6,13 @@
   </head>
   <body>
     <?php require('action.php'); ?>
-    <header> //Website's Header => It will contains Title and main informations
+    <header> <!-- Website's Header => It will contains Title and main informations -->
     </header>
 
-    <nav> //Website's navigation bar => It will contains link to log in, log out, [...]
+    <nav> <!-- Website's navigation bar => It will contains link to log in, log out, [...] -->
     </nav>
 
-    <footer> //Website's footer => It will contains all the informations about the website's owner and contact, policy etc..
+    <footer> <!-- Website's footer => It will contains all the informations about the website's owner and contact, policy etc.. -->
     </footer>
 
   </body>
